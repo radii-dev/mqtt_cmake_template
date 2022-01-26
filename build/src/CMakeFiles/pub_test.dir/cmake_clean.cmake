fa879b33b41eb16f41e8f6afeaacac17cbb1a518
Seongwook Kang <radii@keti.re.kr>
2022-01-26 15:17:56 +0900

    First commit

---
+++ build/src/CMakeFiles/pub_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pub_test.dir/mqtt_pubtest.cpp.o"
+  "pub_test.pdb"
+  "pub_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pub_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
